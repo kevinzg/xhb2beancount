cc32aafb0fb9cf726d76a99cbc6ea5a87f21a4a9
Kevin Zúñiga <kevin@amigocloud.com>
2019-06-22 21:25:14 -0500

    Move version to __version__ module

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/kevinzg/xhb2beancount'
 EMAIL = 'kevin.zun@gmail.com'
 AUTHOR = 'Kevin Zúñiga'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.2'
+VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
--- xhb2beancount/__init__.py
+++ xhb2beancount/__init__.py
@@ -1,1 +1,3 @@
 from .xhb2beancount import convert, overwrite_config, print_dicts
+
+from .__version__ import __version__
